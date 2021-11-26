a93b39e19de3700563a58f40945f2bc261d9c59e
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2021-11-26 06:40:51 +0530

    commit

--- quizbotapp/urls.py
+++ quizbotapp/urls.py
@@ -5,5 +5,5 @@ from quiz.views import RandomQuestion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/random',RandomQuestion.as_view(),name='random'),
+    path('api/random/',RandomQuestion.as_view(),name='random'),
 ]
